7326a75dbde5938b4608874fa2759355dd11842f
Mihail Katsarov <Mihail.Katsarov@devocean.solutions>
2024-09-30 00:16:38 +0300

    Fix AboutPage

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -4,14 +4,12 @@ import SidePanel from "../Components/Side Panel/SidePanel";
 import Header2 from "../Components/Header/Header2";
 import AboutMain from '../Components/Main/AboutMain';
 import Footer from '../Components/Footer/Footer';
-import Login from '../Components/Login/Login';
 import Register from '../Components/Register/Register';
 
 const AboutPage = () => {
   return (
     <>
     <Register/>
-        <Login/>
         <SearchForm/>
         <SidePanel/>
         <Header2/>
