56290d5080f2259a262475ab498bc170a1a216e3
Mihail Katsarov <Mihail.Katsarov@devocean.solutions>
2024-09-30 01:15:24 +0300

    Update register form, rounding, background

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -9,7 +9,7 @@ class Login extends Component {
       Username: "",
       Password: "",
       message: "", // State to hold the message
-    };
+    }; 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
